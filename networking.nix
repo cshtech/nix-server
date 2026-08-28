be353c3b7c27604389cd50de87b8a816ad7e64b4
cshtech <chethan.sh@zohomail.in>
2026-08-29 01:16:43 +0530

    lower the privileged prot range down to 80.
    1. add nginx ports.
    
    2. add goaccess ports.

--- networking.nix
+++ networking.nix
@@ -23,14 +23,30 @@
 
     # SSH Access
     22
+
     # Portainer
     9443
+
     # Immich
     2283
 
+    # Nginx proxy manager
+    # http
+    80
+    # https
+    443
+    # admin panel
+    81
+
+    # goaccess
+    7880 
     
   ];
 
+  # Lower the Privileged Port Range in NixOS
+  # You need to tell the NixOS kernel that regular users
+  # are allowed to bind to ports down to 80.
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
 
 
 
